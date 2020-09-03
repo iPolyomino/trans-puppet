6e31d7f672e3219d404911f1d8217a64b5a0e481
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-09-04 02:06:31 +0900

    Revert "update launch option"
    This reverts commit 2da5ac4fde4b359cf42bd6a683412a744153eb49.

--- google.ts
+++ google.ts
@@ -1,10 +1,9 @@
 import puppeteer from "puppeteer";
 
 export const GoogleTranslate = async (text: string) => {
-  const LAUNCH_OPTION = process.env.DYNO
-    ? { headless: false }
-    : { args: ["--no-sandbox", "--disable-setuid-sandbox"] };
-  const browser = await puppeteer.launch(LAUNCH_OPTION);
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
   await page.goto(
     "https://translate.google.co.jp/#view=home&op=translate&sl=ja&tl=en"
