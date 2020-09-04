ee7423492e105b218d61158e8da67d99b2092934
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-09-04 10:06:05 +0900

    app use

--- index.ts
+++ index.ts
@@ -8,9 +8,12 @@ import { YandexTranslate } from "./yandex";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.all("/", function (req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
